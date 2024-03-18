2a755ea046f8099944f4bfb4df885dcdbc64fba5
sracabanahashas <wkocsis@gmail.com>
2024-03-18 11:16:21 -0700

    Add min-width to container and background colors to each div. Create 16 columns with 16 divs each using a nested for loop in javascript and create a class for both.

--- javascript.js
+++ javascript.js
@@ -2,8 +2,15 @@ console.log("Hello world!");
 
 let container = document.querySelector('#container')
 
-for (i = 0; i < 256; i++) {
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'grey';
-    container.appendChild(square);
+for (i = 0; i < 16; i++) {
+    let column = document.createElement('div')
+    container.appendChild(column);
+    column.classList.add("column");
+    for (j = 0; j < 16; j++) {
+        let square = document.createElement('div')
+        square.style.backgroundColor = 'grey';
+        column.appendChild(square);
+        square.classList.add("square");
+    };
 };
+
