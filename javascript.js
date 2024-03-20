91643c73f2748884592caa33371a73d1120bf1c8
sracabanahashas <wkocsis@gmail.com>
2024-03-20 09:47:41 -0700

    Create button to change grid resolution, prompting for a new number of squares per side and displaying a new container with that parameter

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 console.log("Hello world!");
 
-let container = document.querySelector('#container')
+let container = document.querySelector('.container')
 
 for (i = 0; i < 16; i++) {
     let column = document.createElement('div')
@@ -21,3 +21,24 @@ coloredSquares.forEach((coloredSquare) => (coloredSquare.addEventListener('mouse
     coloredSquare.classList.add("coloredSquare");
 
 })))
+
+let btn = document.querySelector('button');
+let containerContainer = document.querySelector('#containerContainer')
+
+btn.addEventListener('click', () => {
+    let resolution = prompt("Number of squares per side?", "16");
+    let newContainer = document.createElement('div');
+    newContainer.classList.add('container');
+    containerContainer.appendChild(newContainer);
+    
+    for (i = 0; i < resolution; i++) {
+        let column = document.createElement('div')
+        newContainer.appendChild(column);
+        column.classList.add("column");
+        for (j = 0; j < resolution; j++) {
+            let square = document.createElement('div')
+            column.appendChild(square);
+            square.classList.add("square");
+        };
+    };
+})
