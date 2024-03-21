7cb06ae731ecc0130b1d3712bd8997571518a2cc
sracabanahashas <wkocsis@gmail.com>
2024-03-21 08:19:59 -0700

    Change flex basis of squares so larger grids don't take up more space, and add an error condition if a number over 100 is input

--- javascript.js
+++ javascript.js
@@ -26,29 +26,36 @@ let btn = document.querySelector('button');
 let containerContainer = document.querySelector('#containerContainer')
 
 btn.addEventListener('click', () => {
+    
     let resolution = prompt("Number of squares per side?", "16");
-    let newContainer = document.createElement('div');
-    newContainer.classList.add('container');
-    containerContainer.removeChild(containerContainer.lastElementChild);
-    containerContainer.appendChild(newContainer);
     
-    for (i = 0; i < resolution; i++) {
-        let column = document.createElement('div')
-        newContainer.appendChild(column);
-        column.classList.add("column");
-        for (j = 0; j < resolution; j++) {
-            let square = document.createElement('div')
-            column.appendChild(square);
-            square.classList.add("square");
+    if (resolution > 100) {
+        alert('Too many squares!');
+    } 
+    
+    else {
+        let newContainer = document.createElement('div');
+        newContainer.classList.add('container');
+        containerContainer.removeChild(containerContainer.lastElementChild);
+        containerContainer.appendChild(newContainer);
+        
+        for (i = 0; i < resolution; i++) {
+            let column = document.createElement('div')
+            newContainer.appendChild(column);
+            column.classList.add("column");
+            for (j = 0; j < resolution; j++) {
+                let square = document.createElement('div')
+                column.appendChild(square);
+                square.classList.add("square");
+            };
         };
-    };
 
-    let coloredSquares = document.querySelectorAll('.square')
-    console.log(coloredSquares);
+        let coloredSquares = document.querySelectorAll('.square')
+        console.log(coloredSquares);
 
-    coloredSquares.forEach((coloredSquare) => (coloredSquare.addEventListener('mouseover', () => {
-        console.log("Hover");
-        coloredSquare.classList.add("coloredSquare");
-    
-    })))  
+        coloredSquares.forEach((coloredSquare) => (coloredSquare.addEventListener('mouseover', () => {
+            console.log("Hover");
+            coloredSquare.classList.add("coloredSquare");
+        
+        })))  }
 })
